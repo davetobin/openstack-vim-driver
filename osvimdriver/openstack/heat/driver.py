e8a1a7138d791bc7ff6d076e0a4878ec6049abe9
Akshata-Desai <Akshata.Desai@ibm.com>
2023-04-03 05:28:50 -0700

    hamdled None conditions

--- osvimdriver/openstack/heat/driver.py
+++ osvimdriver/openstack/heat/driver.py
@@ -120,9 +120,23 @@ class HeatDriver():
 
     def _generate_additional_logs(self, message_data, message_direction, external_request_id, content_type,
                                   message_type, protocol, protocol_metadata, driver_request_id):
-        try:
+        try:   
             logging_context_dict = {'message_direction' : message_direction, 'tracectx.externalrequestid' : external_request_id, 'content_type' : content_type,
-                                    'message_type' : message_type, 'protocol' : protocol, 'protocol_metadata' : str(protocol_metadata).replace("'", '\"'), 'tracectx.driverrequestid' : driver_request_id}
+                                    'message_type' : message_type, 'protocol' : protocol, 'protocol_metadata' : str(protocol_metadata).replace("'", '\"'),'tracectx.driverrequestid' : driver_request_id }
+            if driver_request_id is None:
+                logging_context_dict.pop('tracectx.driverrequestid')
+            if message_direction is None:
+                logging_context_dict.pop('message_direction')
+            if external_request_id is None:
+                logging_context_dict.pop('tracectx.externalrequestid')
+            if content_type is None:
+                logging_context_dict.pop('content_type') 
+            if message_type is None:
+                logging_context_dict.pop('message_type')
+            if protocol is None:
+                logging_context_dict.pop('protocol')                  
+            if protocol_metadata is None:
+                logging_context_dict.pop('protocol_metadata')  
             logging_context.set_from_dict(logging_context_dict)
             logger.info(str(message_data).replace("'",'\"'))
         finally:
